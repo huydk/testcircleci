e041cfc1c6bf931db4367e5eb6afff5c7500587d
Huy Nguyen <huydongkhe@gmail.com>
2015-12-22 16:23:30 +0700

    dev1 real say a word

--- HelloWorld.java
+++ HelloWorld.java
@@ -5,7 +5,7 @@ public class HelloWorld {
 		System.out.println("Hello World!! from eva");
 		System.out.println("Other command -> XXX");
 		System.out.println("dev_2 is dead (^____^)");
-		System.out.println("dev1 say a word!!!!!!!!!!!!!!!!!");
+		System.out.println("dev1 real -> just make conflict");
 	}
 
 }
