7bb630aa0ba8753d71650751a081ec53b14fabd3
Huy Nguyen <huydongkhe@gmail.com>
2015-12-22 17:42:52 +0700

    remove extra space/tab

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,12 +1,9 @@
-
 public class HelloWorld {
-
-	public static void main(String[] args) {
-		System.out.println("Hello World!! from eva");
-		System.out.println("Other command -> XXX");
-		System.out.println("dev_2 is dead (^____^)");
-		System.out.println("dev1 real -> just make conflict");
-                System.out.println("Hello from dev3 -> nasty developer!!!");
-	}
-
+    public static void main(String[] args) {
+        System.out.println("Hello World!! from eva");
+        System.out.println("Other command -> XXX");
+        System.out.println("dev_2 is dead (^____^)");
+        System.out.println("dev1 real -> just make conflict");
+        System.out.println("Hello from dev3 -> nasty developer!!!");
+    }
 }
