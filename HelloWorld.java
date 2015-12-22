83d45338a490862d08cb9e08b31922ef691b385e
Huy Nguyen <huydongkhe@gmail.com>
2015-12-22 10:27:45 +0000

    dev3 come to say nasty thing

--- HelloWorld.java
+++ HelloWorld.java
@@ -6,6 +6,7 @@ public class HelloWorld {
 		System.out.println("Other command -> XXX");
 		System.out.println("dev_2 is dead (^____^)");
 		System.out.println("dev1 real -> just make conflict");
+                System.out.println("Hello from dev3 -> nasty developer!!!");
 	}
 
 }
