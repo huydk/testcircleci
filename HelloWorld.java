e005710d57e11d141b429b43c50bbfa349b09357
Huy Nguyen <huydongkhe@gmail.com>
2015-12-22 16:15:26 +0700

    dev1 change the code -> commit to cibuild then

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,7 +4,8 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World!! from eva");
 		System.out.println("Other command -> XXX");
-		System.out.println("dev_2 say a word!!!!!!!!!!!!!!!!!");
+		System.out.println("dev_2 is dead (^____^)");
+		System.out.println("dev1 say a word!!!!!!!!!!!!!!!!!");
 	}
 
 }
