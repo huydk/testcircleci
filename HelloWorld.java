64b45d73626e3b818ed9262976f05748644aad73
Huy Nguyen <huydongkhe@gmail.com>
2015-12-22 13:54:31 +0700

    dev 2 remove class and add hiw own sysout

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,6 +4,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World!! from eva");
 		System.out.println("Other command -> XXX");
+		System.out.println("dev_2 say a word!!!!!!!!!!!!!!!!!");
 	}
 
 }
